4ab7574232166ae68bd81891e9d2f0c7e358dbf7
tiaraAinalSalsabilla <tiaraainalsalsabilla@gmail.com>
2018-07-19 23:02:04 +0700

    Initial commit

---
+++ app/src/main/java/com/tiara/placepickermaps/PlacePickerActivity.java
@@ -0,0 +1,53 @@
+package com.tiara.placepickermaps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+public class PlacePickerActivity extends AppCompatActivity {
+
+    int PLACE_PICKER_REQUEST = 1;
+    TextView TvPlace;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_picker);
+
+        TvPlace = (TextView)findViewById(R.id.tvPlace);
+    }
+
+    public void goPlacePicker (View view){
+        //mau manggil function dari place picker
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(PlacePickerActivity.this),PLACE_PICKER_REQUEST);
+        }catch (GooglePlayServicesRepairableException e){
+            e.printStackTrace();
+        }catch (GooglePlayServicesNotAvailableException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        if (requestCode == PLACE_PICKER_REQUEST){
+            if (resultCode == RESULT_OK){
+
+                Place place = PlacePicker.getPlace(PlacePickerActivity.this, data);
+                TvPlace.setText(place.getAddress());
+            }
+        }
+
+    }
+}
